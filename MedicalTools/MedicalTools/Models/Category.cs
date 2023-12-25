505b2c85a53aba37b3dce6f3aa5b90b853fcc6ba
Abedelhameed Alshorafa <abedalshorafa95@gmail.com>
2023-12-25 20:28:08 +0300

    first commit DB && Admin && Home

---
+++ MedicalTools/MedicalTools/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalTools.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
