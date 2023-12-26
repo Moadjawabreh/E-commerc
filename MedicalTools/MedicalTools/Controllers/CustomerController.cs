6f62b11e193fc6049744161b7f04983637188f52
MoadJawabreh <moadjawbreh2001@yahoo.com>
2023-12-26 13:28:04 +0300

    Added Actions And Layout

---
+++ MedicalTools/MedicalTools/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalTools.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult SingleProduct()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
